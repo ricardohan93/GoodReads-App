f11bef1d4ed702f3b46831c0b572483fe55ff781
ricardo <ricardoshhan@gmail.com>
2018-09-16 19:48:40 -0300

    setting up graphql endpoint with schema

--- src/schema.ts
+++ src/schema.ts
@@ -11,6 +11,23 @@ const dataFetch = require("node-fetch");
 const util = require("util");
 const parseXML = util.promisify(require("xml2js").parseString);
 
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    description: "...",
+
+    fields: () => ({
+        title: {
+            type: GraphQLString,
+            resolve: (xml: any): any =>
+                xml.title[0]
+        },
+        isbn: {
+            type: GraphQLString,
+            resolve: (xml: any): any =>
+                xml.isbn[0]
+        }
+    })
+});
 
 const AuthorType = new GraphQLObjectType({
     name: "Author",
@@ -18,7 +35,14 @@ const AuthorType = new GraphQLObjectType({
 
     fields: () => ({
         name: {
-            type: GraphQLString
+            type: GraphQLString,
+            resolve: (xml: any): any =>
+                xml.GoodreadsResponse.author[0].name[0]
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (xml: any): any =>
+                xml.GoodreadsResponse.author[0].books[0].book
         }
     })
 });
